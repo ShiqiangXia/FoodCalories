2e57652fb8844f9774d86294463f08d7867a7ec3
Shiqiang Xia <xiaxx268@umn.edu>
2021-06-25 10:10:18 -0500

    check if there is food in the image

--- img_recog.py
+++ img_recog.py
@@ -66,3 +66,61 @@ def food_dish_detetion(headers, img):
     out = resp.json()['recognition_results'][0]
 
     return out
+
+
+def Clarifai_General_Model2(stub, metadata,file_bytes,model=None, MAX_CONCEPT=10):
+    general_classfier = 'aaa03c23b3724a16a56b629203edc62c'
+    
+    if not model:
+        model = general_classfier
+    post_model_outputs_response = stub.PostModelOutputs(
+        service_pb2.PostModelOutputsRequest(
+            model_id=model,  # This is model ID of the publicly available General model.
+            inputs=[
+                resources_pb2.Input(
+                    data=resources_pb2.Data(
+                        image=resources_pb2.Image(
+                            #url="https://samples.clarifai.com/metro-north.jpg"
+                            base64 = file_bytes
+                        )
+                    )
+                )
+            ],
+            model=resources_pb2.Model(
+                output_info=resources_pb2.OutputInfo(
+                    output_config=resources_pb2.OutputConfig(
+                        language="en",  # 'zh' Chinese
+                        max_concepts=MAX_CONCEPT
+                    )
+                )
+            )
+        ),
+        metadata=metadata
+    )
+
+    if post_model_outputs_response.status.code != status_code_pb2.SUCCESS:
+        raise Exception("Post model outputs failed, status: " + post_model_outputs_response.status.description)
+
+    # Since we have one input, one output will exist here.
+    output = post_model_outputs_response.outputs[0]
+
+    
+    rlt = []
+    for concept in output.data.concepts:
+        rlt.append(concept.name)
+        
+
+    return(rlt)
+    
+def General_Image_Analysis2(file_path,stub,metadata,model = None):
+    
+    image = Image.open(file_path)
+    plt.imshow(image)
+    plt.show()
+    
+    with open(file_path, "rb") as f:
+        img_file = f.read()
+        
+    output = Clarifai_General_Model2(stub, metadata,img_file,model = model);
+    
+    return(output)
